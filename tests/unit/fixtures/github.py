b56f70a2d2b6ad0500340735696460af96d3bb9b
ye11ow <ye111111ow@gmail.com>
2020-02-25 23:21:18 -0800

    Add Github UT

---
+++ tests/unit/fixtures/github.py
@@ -0,0 +1,69 @@
+from unittest.mock import MagicMock
+
+from dateutil import parser
+
+class DummyComment:
+
+    def __init__(self):
+        self._user = MagicMock()
+        self._user.login = '_author_'
+    
+    @property
+    def user(self):
+        return self._user
+
+    @property
+    def created_at(self):
+        return parser.parse('2020-02-02T20:20:20Z')
+
+    @property
+    def body(self):
+        return '_body_'
+    
+    @property
+    def html_url(self):
+        return '_url_'
+
+class DummyPR:
+
+    def __init__(self, comments=[]):
+        self._head = MagicMock()
+        self._head.ref = '_branch_'
+        self._head.sha = '_sha_'
+        self._comments = comments
+
+    def get_comments(self):
+        return self._comments
+
+    @property
+    def title(self):
+        return '_title_'
+
+    @property
+    def html_url(self):
+        return '_url_'
+
+    @property
+    def head(self):
+        return self._head
+    
+    @property
+    def mergeable(self):
+        return True
+
+class DummyBuild:
+
+    def __init__(self, state='success'):
+        self._state = state
+    
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def context(self):
+        return '_name_'
+
+    @property
+    def target_url(self):
+        return '_url_'
---
+++ tests/unit/test_github_pr.py
@@ -0,0 +1,51 @@
+from unittest.mock import MagicMock
+import pytest
+
+from noti import GithubPR
+from fixtures import github as mock
+
+class TestGithubPR:
+
+    @pytest.fixture
+    def default_pr(self):
+        return GithubPR(MagicMock(), mock.DummyPR())
+
+    def test_title(self, default_pr):
+        assert default_pr.title == '_title_'
+    
+    def test_ci_status(self, default_pr):
+        default_pr._repo.get_commit.return_value.get_combined_status.return_value.state = 'success'
+        assert default_pr.ci_status == 'success'
+        default_pr._repo.get_commit.assert_called_with('_sha_')
+        default_pr._repo.get_commit.return_value.get_combined_status.assert_called_once()
+
+    def test_failed_pipeline_jobs(self, default_pr):
+        default_pr._status = MagicMock()
+        default_pr._status.statuses = [
+            mock.DummyBuild('failure'),
+            mock.DummyBuild(),
+            mock.DummyBuild('failure'),
+            mock.DummyBuild(),
+            mock.DummyBuild('failure')
+        ]
+
+        assert len(default_pr.failed_pipeline_jobs) == 3
+
+    def test_approved(self, default_pr):
+        assert default_pr.approved
+
+    def test_url(self, default_pr):
+        assert default_pr.url == '_url_'
+    
+    def test_branch(self, default_pr):
+        assert default_pr.branch == '_branch_'
+
+    def test_reviews(self):
+        comments = [
+            mock.DummyComment(),
+            mock.DummyComment(),
+            mock.DummyComment()
+        ]
+        pr = GithubPR(MagicMock(), mock.DummyPR(comments=comments))
+
+        assert len(pr.reviews) == 3
