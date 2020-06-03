41c3ccde45f392bb9df50d375961f0198c680676
ye11ow <ye111111ow@gmail.com>
2020-06-02 22:11:55 -0700

    Added UT for Gitlab filter

--- tests/unit/test_gitlab.py
+++ tests/unit/test_gitlab.py
@@ -6,6 +6,11 @@ import pytest
 from noti import Gitlab
 from noti import NotiError
 
+def build_mr_with_username(username): 
+    mr = MagicMock()
+    mr.author.get.return_value = username
+    return mr
+
 class TestGitlab:
 
     @pytest.fixture(autouse=True)
@@ -15,6 +20,18 @@ class TestGitlab:
             "project_id": [1],
             "host": "https://example.com",
             "mr_limit": 5
+        }
+
+    @pytest.fixture(autouse=True)
+    def config_with_filter(self):
+        return {
+            "token": "fake",
+            "project_id": [1],
+            "host": "https://example.com",
+            "mr_limit": 5,
+            "filters": {
+                "usernames": ["test1", "test2"]
+            }
         }
 
     def test_init(self, config):
@@ -44,3 +61,18 @@ class TestGitlab:
         project.attributes.get.assert_called_with('name')
         assert len(mrs['test_repo']) == 3
         project.mergerequests.list.assert_called_with(state='opened', per_page=5)
+
+    @patch('gitlab.Gitlab')
+    def test_get_mrs_with_filter(self, mock_gl, config_with_filter):
+        project = MagicMock()
+        mock_gl.return_value.projects.get.return_value = project
+        project.mergerequests.list.return_value = [build_mr_with_username('test1'), build_mr_with_username('test2'),  build_mr_with_username('test3')]
+        project.attributes.get.return_value = 'test_repo'
+
+        g = Gitlab(config_with_filter)
+        mrs = g.get_mrs()
+
+        assert 'test_repo' in mrs
+        project.attributes.get.assert_called_with('name')
+        assert len(mrs['test_repo']) == 2
+        project.mergerequests.list.assert_called_with(state='opened', per_page=100)
